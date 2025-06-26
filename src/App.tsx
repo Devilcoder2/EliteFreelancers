bacaf63a1386f4c8eef0946453b34ea37902cb62
Ramandeep Singh <113927776+Devilcoder2@users.noreply.github.com>
2025-06-26 16:38:10 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
-const App = () => {
-  return <div className="bg-red-600">this is app</div>
+import FAQ from './components/FAQ';
+import Footer from './components/Footer';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+import OurWork from './components/OurWork';
+import Pricing from './components/Pricing';
+import Testimonials from './components/Testimonals';
+import WhyUs from './components/WhyUs';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-900">
+      <Navbar />
+      <Hero />
+      <WhyUs />
+      <OurWork />
+      <Testimonials />
+      <Pricing />
+      <FAQ />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
